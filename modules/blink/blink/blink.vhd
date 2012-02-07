c28397c4a222ed92af6900903e92daa1a969c5fb
Oswald Berthold <oberthold@informatik.hu-berlin.de>
2012-02-07 12:58:49 +0100

    - cleaned up blink examples

--- modules/blink/blink/blink.vhd
+++ modules/blink/blink/blink.vhd
@@ -1,39 +1,13 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    16:02:06 07/22/2010 
--- Design Name: 
--- Module Name:    blink - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+-- wrap a very simple blink module for use in an EDK
+-- based microprocessor project
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity blink is
   Port(
-    clkin : in  STD_LOGIC;
---    rst : in  STD_LOGIC;
-    led : out  STD_LOGIC
+    clkin : in  STD_LOGIC;              -- clock it
+    led : out  STD_LOGIC                -- single bit LED output
   );
 end blink;
 
@@ -41,7 +15,6 @@ architecture Behavioral of blink is
   component blink_act
     port (
       clkin : in  std_logic;              -- clock
---      rst : in  std_logic;              -- reset
       led : out std_logic);             -- LED driver
   end component;
 
@@ -51,20 +24,15 @@ begin
   -- user logic
   blink_instance : blink_act
     port map (
-      clkin => clkin,
---      rst => rst,
--- changed 20100729
+      clkin => clkin,                   -- clock is unregistered
       led => led_reg
---      led => led
     );
 
--- changed 20100729
   reg_proc : process(clkin)
-    begin
-      if rising_edge(clkin) then
-        led <= led_reg;
-      end if;
-    end process reg_proc;
-    
+  begin
+    if rising_edge(clkin) then
+      led <= led_reg;                   -- transfer to output port
+    end if;
+  end process reg_proc;
+  
 end Behavioral;
-
--- modules/blink/blink_static/blink_static.vhd
+++ modules/blink/blink_static/blink_static.vhd
@@ -1,34 +1,9 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    15:35:39 07/22/2010 
--- Design Name: 
--- Module Name:    blink_fast_nr - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+-- Static blink module implementation for LED blink module
+-- high frequency
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.all;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity blink_static is
   Port (
